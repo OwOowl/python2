ed74c22b53dddb685ec03a2a4122b0354a548406
OwOowl <ymh0052@naver.com>
2022-12-06 16:32:30 +0900

    공개 api 크롤링

---
+++ ch05/openapi_seoul.py
@@ -0,0 +1,71 @@
+import os, sys
+import urllib.request
+import datetime, time
+import json
+import pandas as pd
+
+
+serviceKey = "5155506b6173696d37357446486963"
+
+
+# [CODE 1]
+def getRequestUrl(url):
+    req = urllib.request.Request(url)
+    try:
+        response = urllib.request.urlopen(req)
+        if response.getcode() == 200:
+            print ("[%s] Url Request Success" % datetime.datetime.now())
+            return response.read().decode('utf-8')
+    except Exception as e:
+        print(e)
+        print("[%s] Error for URL : %s" % (datetime.datetime.now(), url))
+        return None
+
+
+# [CODE 2] '서울 열린테이터 광장 : 한 page에 5개만 display됨.
+def getPage(start):
+    url = 'http://openapi.seoul.go.kr:8088/' + serviceKey + '/json/ChunmanFreeSuggestions/%d/%d' % (start, start + 4)
+    retData = getRequestUrl(url)  # [CODE 1]
+
+    if (retData == None):
+        return None
+    else:
+        return json.loads(retData)
+
+
+#[CODE 3]  "서울 열린데이터 광장" : 최대 1000개까지만 제공
+def getItemsAll():
+    result = []
+    for i in range(1000//5):
+        jsonData = getPage(i*5 +1) #[CODE 2]
+        if (jsonData['ChunmanFreeSuggestions']['RESULT']['CODE'] == 'INFO-100'):
+            print("인증키가 유효하지 않습니다!!")
+            return
+
+        if(jsonData['ChunmanFreeSuggestions']['RESULT']['CODE'] == 'INFO-000'):
+            for i in range(5):
+                SN = jsonData['ChunmanFreeSuggestions']['row'][i]['SN']
+                TITLE = jsonData['ChunmanFreeSuggestions']['row'][i]['TITLE']
+                CONTENT_link = jsonData['ChunmanFreeSuggestions']['row'][i]['CONTENT']
+                DATE = jsonData['ChunmanFreeSuggestions']['row'][i]['REG_DATE']
+                result.append([SN, TITLE, CONTENT_link, DATE])
+    return result
+
+
+# [CODE 0]
+def main():
+    jsonResult = []
+    result = []
+
+    print("<< 현재 기준 '민주주의 서울 자유 제안' 데이터 1000개를 수집합니다. >>")
+    result = getItemsAll()
+
+#     파일 저장 csv
+    columns = ['SN', 'TITLE', 'CONTENT_lick', 'DATE']
+    result_df = pd.DataFrame(result, columns=columns)
+    result_df.to_csv('./data/민주주의서울자유제안.csv', index=False, encoding='cp949')
+
+
+if __name__ == '__main__':
+    main()
+
